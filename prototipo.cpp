c1bb8ccb0f286dbf3163a31d4f212cc86d6febf0
Felie Cortez dos Santos <145861403+fczs0@users.noreply.github.com>
2023-10-27 08:27:17 -0300

    Update prototipo.cpp
    V2

--- prototipo.cpp
+++ prototipo.cpp
@@ -1,29 +1,30 @@
-#include "Arduino.h"
+#define deviceID = 1 //ID do dispositivo
+#define DHTPIN 15 // Pino onde o sensor DHT11 está conectado
+#define DHTTYPE DHT11 // Tipo do sensor DHT
+#define REED_SWITCH 13 // Pino onde o reed switch está conectado
+#define arraySize 4 // Definindo o tamanho da Array
+/* Definindo regra de status return */
+#define TIPO_CHUVA 1
+#define TIPO_TEMPERATURA 2
+#define TIPO_UMIDADE 3
+/* Incluindo bibliotecas */
 #include "WiFi.h"
 #include "HTTPClient.h"
 #include "DHT.h"
-
-#define deviceID [] = "01"; //ID do dispositivo
-#define DHTPIN 13 // Pino onde o sensor DHT11 está conectado
-#define DHTTYPE DHT11 // Tipo do sensor DHT
-#define REED_SWITCH_PIN 15 // Pino onde o reed switch está conectado
-#define arraySize 4 // Definindo o tamanho da Array
-
-char ssid[] = "Galaxy Felipe";
-char pass[] = "Felipe100603";
+char ssid[] = "Galaxy Felipe"; // Nome da rede WiFi
+char pass[] = "Felipe100603"; // Senha da rede WiFi
 char serverAddress[] = "https://api.tago.io/data";  // TagoIO address
 char contentHeader[] = "application/json";
 char tokenHeader[]   = "b6649bfb-bca8-4ee5-8cf6-5b8f021f4621"; // TagoIO Token
-
 HTTPClient client; // Iniciar uma nova instância do cliente HTTP
 DHT dht(DHTPIN, DHTTYPE);
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin (9600);
   init_wifi();
   pinMode(DHTPIN, INPUT);
   dht.begin(); // Inicializar o sensor DHT11
-  pinMode(REED_SWITCH_PIN, INPUT_PULLUP); // Configurar o reed switch como entrada com resistor pull-up
+  pinMode (REED_SWITCH, INPUT);
+  attachInterrupt(digitalPinToInterrupt(REED_SWITCH), count, FALLING); //Debouncing Reed Switch
 }
 void init_wifi() {
   Serial.println("Conectando WiFi");
@@ -36,122 +37,143 @@ void init_wifi() {
   Serial.print("Meu IP eh: ");
   Serial.println(WiFi.localIP());
 }
-
-int ReedSwitch = digitalRead(REED_SWITCH_PIN); // Variável do Reed Switch
-float Umidade = dht.readHumidity(); // Variável de umidade
-float Temperatura = dht.readTemperature(); // Variável de temperatura
-
-float Chuva = 0.0;
-float ValueChuva[arraySize] = {0, 0, 0, 0};
-float ChuvaHora = 0;
-float AddChuva = 1.6;
-
-long Inicio = 0; // Tempo do inicio do aparelho
-long Intervalo = 1 * 60 * 1000; // Controle de Minutos
-int AnteriorRS = LOW; // Inicialize com um valor conhecido
-int contador = 0;
-
+char anyData[30];
+char chuvaData[300];
+char umidadeData[300];
+char tempData[300];
+int statusCode = 0;
+float temperatura = 0;
+float umidade = 0;
+float chuva = 0;
+float addChuva = 1.6;
+float chuvaH = 0;
+bool reedStats = false;
+float lastChuva [arraySize]  = {0, 0, 0, 0};
+int arrayCounter = 0;
+long controle = 0; // Tempo do inicio do aparelho
+long intervalo = 5 * 1000; // Controle de Minutos
+// Função para obter o status com base no valor
+void getStatus(float value, int variableName, char* retorno) {
+  switch (variableName) {
+    case TIPO_CHUVA:
+      if (value >= 15.0) {
+        strcpy(retorno, ";ChuvaForte");
+      } else if (value >= 5.0) {
+        strcpy(retorno, ";ChuvaMedia");
+      } else if (value > 0) {
+        strcpy(retorno, ";ChuvaFraca");
+      } else {
+        strcpy(retorno, ";ChuvaNull");
+      }
+      break;
+    case TIPO_TEMPERATURA:
+      if (value >= 30.0) {
+        strcpy(retorno, ";TemperaturaAlta");
+      } else if (value >= 17.0) {
+        strcpy(retorno, ";TemperaturaBoa");
+      } else if (value > 0) {
+        strcpy(retorno, ";TemperaturaBaixa");
+      }
+      break;
+    case TIPO_UMIDADE:
+      if (value >= 80.0) {
+        strcpy(retorno, ";UmidadeAlta");
+      } else if (value >= 50.0) {
+        strcpy (retorno, ";UmidadeBoa");
+      } else if (value > 0) {
+        strcpy(retorno, ";UmidadeBaixa");
+      }
+      break;
+  }
+}
+void count(void) {
+  if (!reedStats) {
+    reedStats = true;
+    chuva += addChuva;
+  }
+}
 void loop() {
-  long Agora = millis();  // Obtém o tempo atual
-  ReedSwitch;
-  
-  char anyData[30];
-  char anyData1[30];
-  char chuvaData[300];
-  char umidadeData[300];
-  char tempData[300];
-  int statusCode = 0;
+  long agora = millis();  // Obtém o tempo atual
+  float umidade = dht.readHumidity(); // Variável de umidade
+  float temperatura = dht.readTemperature(); // Variável de temperatura
+  if (reedStats) {
+    delay(300);
+    reedStats = false;
+    Serial.print("chuva: ");
+    Serial.println (chuva);
+    Serial.print("umidade: ");
+    Serial.println (umidade);
+    Serial.print("temperatura: ");
+    Serial.println (temperatura);
+  }
+  if (agora - controle >= intervalo) {
+    if (arrayCounter >= arraySize) {
+      arrayCounter = 0;
+      for (int i = 0; i < arraySize; i++) {
+        chuvaH = chuvaH + lastChuva[i];
+      }
+      Serial.print("Soma = "); Serial.println(chuvaH);
+      /* Mandando dados para TAGO */
+      char varStatus[100];
+      /* Informações de Chuva*/
+      strcpy(chuvaData, "{\n\t\"variable\": \"Chuva\",\n\t\"value\": \"");
+      dtostrf(chuvaH, 6, 2, anyData);
+      strncat(chuvaData, anyData, 100);
+      getStatus(chuvaH, TIPO_CHUVA, varStatus);
+      strncat(chuvaData, varStatus, 100);
+      strcat(chuvaData, "\"\n\t}\n");
+      client.begin(serverAddress);
+      client.addHeader("Content-Type", contentHeader);
+      client.addHeader("Device-Token", tokenHeader);
+      statusCode = client.POST(chuvaData);
+      Serial.print("Informações de Chuva: ");
+      Serial.println(chuvaData);
+      Serial.println(statusCode);
 
-  if (ReedSwitch != AnteriorRS) { // Houve uma mudança no estado do reed switch
-    if (ReedSwitch == HIGH) {
-      // O reed switch mudou para HIGH (contato fechado)
-      Chuva += AddChuva;
-    } else {
-      // O reed switch mudou para LOW (contato aberto)
-      Chuva += AddChuva;
+      /* Informações de Temperatura*/
+      strcpy(tempData, "{\n\t\"variable\": \"Umidade\",\n\t\"value\": \"");
+      strcpy(tempData, "{\n\t\"variable\": \"Temperatura\",\n\t\"value\": \"");
+      dtostrf(temperatura, 6, 2, anyData);
+      strncat(tempData, anyData, 100);
+      getStatus(temperatura, TIPO_TEMPERATURA, varStatus);
+      strncat(tempData, varStatus, 100);
+      strcat(tempData, "\"\n\t}\n");
+      client.begin(serverAddress);
+      client.addHeader("Content-Type", contentHeader);
+      client.addHeader("Device-Token", tokenHeader);
+      statusCode = client.POST(tempData);
+      Serial.print("Informações de temperatura: ");
+      Serial.println(tempData);
+      Serial.println(statusCode);
+
+      /* Informações de Umidade*/
+      strcpy(umidadeData, "{\n\t\"variable\": \"Chuva\",\n\t\"value\": \"");
+      strcpy(umidadeData, "{\n\t\"variable\": \"Umidade\",\n\t\"value\": \"");
+      dtostrf(umidade, 6, 2, anyData);
+      strncat(umidadeData, anyData, 100);
+      getStatus(umidade, TIPO_UMIDADE, varStatus);
+      strncat(umidadeData, varStatus, 100);
+      strcat(umidadeData, "\"\n\t}\n");
+      client.begin(serverAddress);
+      client.addHeader("Content-Type", contentHeader);
+      client.addHeader("Device-Token", tokenHeader);
+      statusCode = client.POST(umidadeData);
+      Serial.print("Informações de umidade: ");
+      Serial.println(umidadeData);
+      Serial.println(statusCode);
+      chuvaH = 0;
     }
-    AnteriorRS = ReedSwitch; // Atualize o estado anterior
-  }
-
-  if (Agora - Inicio >= Intervalo) { // Passou o intervalo definido
-
-    if (contador >= arraySize) {
-            contador = 0;
-            ValueChuva[contador] = Chuva;
-    } else {
-            ValueChuva [contador] = Chuva;
+    lastChuva[arrayCounter] = chuva;
+    arrayCounter = arrayCounter + 1;
+    chuva = 0;
+    Serial.print("lastChuva: [");
+    for (int i = 0; i < arraySize; i++) {
+      Serial.print(lastChuva[i]);
+      if (i < arraySize - 1) {
+        Serial.print(", ");
+      }
     }
-        contador = contador + 1;
-        for (int i = 0; i < 4; i++) {
-            ChuvaHora += ValueChuva[i];
-        } 
-        
-  // Formatar e enviar dados de Chuva/Hora
-  strcpy(chuvaData, "{\n\t\"variable\": \"Chuva\",\n\t\"value\": ");
-  dtostrf(ChuvaHora, 6, 2, anyData);
-  strncat(chuvaData, anyData, 100);
-  strcpy(anyData1, ",\n\t\"unit\": \"mm/h\"\n\t}\n");
-  strncat (chuvaData, anyData1, 100);
-  Serial.println(chuvaData);
-  client.begin(serverAddress);
-  client.addHeader("Content-Type", contentHeader);
-  client.addHeader("Device-Token", tokenHeader);
-  statusCode = client.POST(chuvaData);
-  delay (2000);
-  Serial.print("Status code (Chuva): ");
-  Serial.println(statusCode);
-  Serial.println("End of POST to TagoIO");
-  Serial.println();
-
-  // Formatar e enviar dados de Temperatura
-  strcpy(tempData, "{\n\t\"variable\": \"Temperatura\",\n\t\"value\": ");
-  dtostrf(Temperatura, 6, 2, anyData);
-  strncat(tempData, anyData, 100);
-  strcpy(anyData1, ",\n\t\"unit\": \"ºC\"\n\t}\n");
-  strncat (tempData, anyData1, 100);
-  Serial.println(tempData);
-  client.begin(serverAddress);
-  client.addHeader("Content-Type", contentHeader);
-  client.addHeader("Device-Token", tokenHeader);
-  statusCode = client.POST(tempData);
-  delay (2000);
-  Serial.print("Status code (Temperatura): ");
-  Serial.println(statusCode);
-  Serial.println("End of POST to TagoIO");
-  Serial.println();
-  delay(5000);
-
-  // Formatar e enviar dados de Umidade
-  strcpy(umidadeData, "{\n\t\"variable\": \"Umidade\",\n\t\"value\": ");
-  dtostrf(Umidade, 6, 2, anyData);
-  strncat(umidadeData, anyData, 100);
-  strcpy(anyData1, ",\n\t\"unit\": \"%\"\n\t}\n");
-  strncat(umidadeData, anyData1, 100);
-  Serial.println(umidadeData);
-  client.begin(serverAddress);
-  client.addHeader("Content-Type", contentHeader);
-  client.addHeader("Device-Token", tokenHeader);
-  statusCode = client.POST(umidadeData);
-   Serial.print("Status code (Umidade): ");
-  Serial.println(statusCode);
-  Serial.println("End of POST to TagoIO");
-
-  Inicio = Agora;  // Atualiza o tempo da última reinicialização
-  
-  Serial.print("ValueChuva: ");
-  for (int i = 0; i < 4; i++) {
-    Serial.print(ValueChuva[i]);
-    Serial.print(", ");
+    Serial.println("]");
+    controle = millis();
   }
-  Serial.println();
-  Serial.print("Chuva: ");
-  Serial.println (Chuva);
-  Serial.print("ChuvaHora: ");
-  Serial.println (ChuvaHora);
-  Serial.print("Umidade: ");
-  Serial.println (Umidade);
-  Serial.print("Temperatura: ");
-  Serial.println (Temperatura);
-  }
-  
 }
